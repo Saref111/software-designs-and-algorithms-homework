3597705430d5923cbe63eef746a5dbd5e20f143c
Ivan Borovyk <ivan_borovyk@epam.com>
2023-02-17 18:27:39 +0200

    created Shape

--- building-blocks-of-oop-part-1-homework/src/Shape.ts
+++ building-blocks-of-oop-part-1-homework/src/Shape.ts
@@ -1,3 +1,35 @@
+import { Point } from "./Point";
+
 export abstract class Shape {
+  protected color: string;
+  protected filled: boolean;
+  protected points: Point[];
+
+  constructor(points: Point[]);
+  constructor(points: Point[], color: string = 'green', filled: boolean = true) {
+    if (points.length < 3) {
+      throw 'Not enough points to build a shape';
+    }
+
+    this.points = points;
+    this.color = color;
+    this.filled = filled;
+  }
+
+  toString() {
+    return `A Shape with color of ${this.color} and ${this.filled ? 'filled' : 'not filled'}. Points: ${
+      this.points.map((p) => p.toString()).join(', ')
+    }.`;
+  }
+
+  getPerimeter() {
+    return this.points.reduce((acc, point, i) => {
+      if (i < this.points.length - 1) { 
+        return acc + point.distance(this.points[i + 1]);
+      }
+      return acc + point.distance(this.points[0]);
+    }, 0)
+  }
+
   abstract getType(): string;
 }
