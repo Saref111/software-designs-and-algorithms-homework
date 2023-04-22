8c8557f4bac5ce5d39253ccc5584353fa806a89c
Ivan Borovyk <ivan_borovyk@epam.com>
2023-04-22 12:48:19 +0300

    done

--- algorithms-and-data-structures-part-2/index.ts
+++ algorithms-and-data-structures-part-2/index.ts
@@ -1,19 +1,41 @@
+const PRIORITY = 100;
+const MAX_JOBS = 10000;
+
 class JobRunner {
     private jobs: Job[] = [];
-    private isRunning = false;
+    private priorityMap: Map<number, Job[]> = new Map();
     
     constructor() {
-        for (let i = 0; i < 10000; i++) {
+        for (let i = 0; i < MAX_JOBS; i++) {
             this.jobs.push(new Job());
         }
     }
 
-    sortJobsByPriority() {}
+    sortJobsByPriority() {
+        this.jobs.forEach((job) => {
+            if (this.priorityMap.has(job.priority)) {
+                this.priorityMap.get(job.priority)!.push(job);
+            } else {
+                this.priorityMap.set(job.priority, [job]);
+            }
+        });
+    }
+
+    sortMapByPriority() {
+        const sortedMap = new Map([...this.priorityMap.entries()].sort((a, b) => b[0] - a[0]));
+        this.priorityMap = sortedMap;
+    }
     
     run() {
-        this.jobs.forEach(job => {
-            job.run();
+        console.time();
+        this.sortJobsByPriority();
+        this.sortMapByPriority();
+        this.priorityMap.forEach((jobs, priority) => {
+            jobs.forEach((job) => {
+                job.run();
+            });
         });
+        console.timeEnd();
     }
 }
 
@@ -21,11 +43,16 @@ class JobRunner {
 class Job {
     priority: number;
     constructor() {
-        this.priority = Math.random();
+        this.priority = Math.floor(Math.random() * PRIORITY);
     }
     
     run() {
         console.log('Running job with priority', this.priority);
-        
+        const result = Math.random() * 10;    
+        return result;    
     }
 }
+
+
+const jobRunner = new JobRunner();
+jobRunner.run();
